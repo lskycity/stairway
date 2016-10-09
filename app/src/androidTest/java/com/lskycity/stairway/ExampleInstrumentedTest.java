5030d67f549ef6afdb31a63dc1850aee206160d4
zhaofliu <lskycity@foxmail.com>
2016-10-09 17:04:17 +0800

    rename the package name

--- app/src/androidTest/java/com/lskycity/stairway/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/lskycity/stairway/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.lskycity.stairway.stairway", appContext.getPackageName());
+        assertEquals("com.lskycity.stairway", appContext.getPackageName());
     }
 }
