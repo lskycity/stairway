16d8c7d0f38f7c7c835f88cb7cd42a164ba24803
zhaofliu <lskycity@foxmail.com>
2016-10-09 15:01:25 +0800

    first commit

---
+++ app/src/main/java/com/lskycity/stairway/stairway/MainActivity.java
@@ -0,0 +1,13 @@
+package com.lskycity.stairway.stairway;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
